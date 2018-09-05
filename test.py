63b73d4906d936bf27427c83a1f01a5eb0677e91
cheapthrillandwine <cheapthrill.and.wine@gmail.com>
2018-09-05 11:15:54 +0900

    second commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-
-if __name__ == "__main__":
-    fp = open(os.path.join(r"./", "test3.csv"), 'w')
-    writer= csv.DictWriter(fp, fieldnames = ["DateTime"])
-    writer.writeheader()
-    writer.writerow({'DateTime': 1})
-
